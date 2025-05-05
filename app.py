564c9cc28b636986738ab457c9d5478e8e598acc
youssefsouaidi <161989963+youssefsouaidi@users.noreply.github.com>
2025-05-05 14:32:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request, jsonify
 import requests
 
 app = Flask(__name__)
@@ -7,17 +6,25 @@ app = Flask(__name__)
 @app.route("/tableau-token", methods=["GET"])
 def get_token():
     try:
+        token_name = request.args.get("token_name")
+        token_secret = request.args.get("token_secret")
+
+        if not token_name or not token_secret:
+            return jsonify({"error": "Missing token_name or token_secret in parameters"}), 400
+
         payload = {
             "credentials": {
-                "personalAccessTokenName": os.environ.get("TABLEAU_TOKEN_NAME"),
-                "personalAccessTokenSecret": os.environ.get("TABLEAU_TOKEN_SECRET"),
+                "personalAccessTokenName": token_name,
+                "personalAccessTokenSecret": token_secret,
                 "site": { "contentUrl": "" }
             }
         }
         headers = { "Content-Type": "application/json" }
+
         response = requests.post("https://api.tableau.com/api/3.19/auth/signin", json=payload, headers=headers)
         response.raise_for_status()
         return jsonify(response.json())
+
     except Exception as e:
         print("❌ Erreur :", e)
         return jsonify({"error": "Internal error", "details": str(e)}), 500
