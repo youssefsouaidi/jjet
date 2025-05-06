f2c5dd40df8e0bbd8539f22c6b310052c553bd82
youssefsouaidi <161989963+youssefsouaidi@users.noreply.github.com>
2025-05-06 17:26:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 # Configuration — à remplacer par tes vraies infos
-CLIENT_ID = "cf67633f-3bae-486e-af25-c004a3938d43"
+CLIENT_ID = "1499c554-9265-4588-90c1-dcb83c4e278e"
 SECRET = "eVqar2O97az0CWfqbEHJdnircKh17dfUGD1BerlTVM0"
 ISSUER = CLIENT_ID  # en général, c’est le client_id
 AUDIENCE = "https://prod-uk-a.online.tableau.com"
